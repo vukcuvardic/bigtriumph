d213533b41940d13678d3c2a8f9899aeb583fdd0
mr.wolf <vukcuvardic@gmail.com>
2016-09-25 08:05:16 +0200

    Previous changes

--- about-us/index.php
+++ about-us/index.php
@@ -12,27 +12,27 @@ include('../includes/header.php');
     <section class="our-vision">
         <h2>Our Vision</h2>
         <p>
-            <strong>Family</strong>, base of every society. <strong>Home</strong>, place where we 
+            <strong>Family</strong>, base of every society. <strong>Home</strong>, place where we
             raise our children to become happy, healthy and independent. Giving them
-            good eating habits from early age and teaching them to be active and 
-            stay healthy is major task for us as parents. How could we prevent our 
-            bodies from being unhealthy, tired, overweight, and unwell? How could we 
+            good eating habits from early age and teaching them to be active and
+            stay healthy is major task for us as parents. How could we prevent our
+            bodies from being unhealthy, tired, overweight, and unwell? How could we
             get out of a circle that bring us back to where we started over and over
             again?
         </p>
         <p>
-            We have all the answers. We are not going to be the slaves to any diet 
-            or food regime where you calculate, measure, count… We will simply 
+            We have all the answers. We are not going to be the slaves to any diet
+            or food regime where you calculate, measure, count… We will simply
             follow natural path and give our body all kinds of food.
         </p>
         <p>
             We are what we eat. This entire journey will be focused on learning how
-            to eat well and simply get a final product which is NEW YOU. It’s all 
-            about using the right ingredients and consume them in the right time. 
+            to eat well and simply get a final product which is NEW YOU. It’s all
+            about using the right ingredients and consume them in the right time.
             We are working with no pressure, we don’t do diet, we are setting you up
-            for a good health for many years to come. It’s fun for everyone. 
-            Adults will be using most recent and high effective ways of getting in 
-            shape with a help of our trendy fitness professionals while kids will be 
+            for a good health for many years to come. It’s fun for everyone.
+            Adults will be using most recent and high effective ways of getting in
+            shape with a help of our trendy fitness professionals while kids will be
             getting fit through play and different sport activities.
         </p>
     </section><!-- /.our-vision -->
@@ -56,8 +56,8 @@ include('../includes/header.php');
                 <div class="row">
                     <div class="member_image">
                         <img data-interchange="
-                             [../dist/images/our-team/marina-krizanovic-305.jpg, (small)], 
-                             [../dist/images/our-team/marina-krizanovic-305.jpg, (medium)], 
+                             [../dist/images/our-team/marina-krizanovic-305.jpg, (small)],
+                             [../dist/images/our-team/marina-krizanovic-305.jpg, (medium)],
                              [../dist/images/our-team/marina-krizanovic-225.jpg, (large)]"
                              alt="Marina Krizanovic" />
                     </div>
@@ -82,53 +82,11 @@ include('../includes/header.php');
 
                 <div class="member_desc">
                     <p>
-                        Based in Dubai, for almost 10 years, as a Fitness Professional 
-                        and Food Engineer, I observed that many people struggle to find 
-                        the way to lose weight, eat healthy and change their lifestyle 
-                        in general. And, here we are. Brand new concept created by 
+                        Based in Dubai, for almost 10 years, as a Fitness Professional
+                        and Food Engineer, I observed that many people struggle to find
+                        the way to lose weight, eat healthy and change their lifestyle
+                        in general. And, here we are. Brand new concept created by
                         seeing people, living with them and understanding their needs.
-                    </p>
-                </div>
-
-            </div>
-
-        </section><!-- /.member -->
-
-        <section class="our-team_member">
-            <h4 class="member_role">
-                <strong>Dietitian</strong>
-            </h4>
-
-            <div class="member_details">
-
-                <div class="row">
-                    <div class="member_image">
-                        <img data-interchange="
-                             [../dist/images/our-team/rouba-kahil-305.jpg, (small)], 
-                             [../dist/images/our-team/rouba-kahil-305.jpg, (medium)], 
-                             [../dist/images/our-team/rouba-kahil-225.jpg, (large)]"
-                             alt="Rouba Kahil" />
-                    </div>
-                    <div class="member_bio">
-                        <h3>Rouba Kahil</h3>
-                        <ul class="member_highlighs">
-                            <li>
-                                <strong>Bachelor degree in Nutrition and Food Science</strong>
-                            </li>
-                        </ul>                        
-                    </div>
-                </div>
-
-                <div class="member_desc">
-                    <p>
-                        Working as a clinical dietitian in Dubai for the last 5 
-                        years, helping people <strong>maintain</strong> a healthy lifestyle and 
-                        creating individual diet plans.
-                        Having experience with elderly, children and very complex situations.
-                    </p>
-                    <p>Big Triumph is a very excited program that provides 
-                        families full lifestyle package. We begin by 
-                        incorporating nourishing, healthy options in the kitchen.
                     </p>
                 </div>
 
@@ -146,8 +104,8 @@ include('../includes/header.php');
                 <div class="row">
                     <div class="member_image">
                         <img data-interchange="
-                             [../dist/images/our-team/branko-brescanski-305.jpg, (small)], 
-                             [../dist/images/our-team/branko-brescanski-305.jpg, (medium)], 
+                             [../dist/images/our-team/branko-brescanski-305.jpg, (small)],
+                             [../dist/images/our-team/branko-brescanski-305.jpg, (medium)],
                              [../dist/images/our-team/branko-brescanski-225.jpg, (large)]"
                              alt="Branko Brescanski" />
                     </div>
@@ -172,10 +130,10 @@ include('../includes/header.php');
 
                 <div class="member_desc">
                     <p>
-                        After many years of training, work experience and 
-                        learning, I always asked myself: Is this the best 
-                        version of myself and the best I can give; 
-                        through my accomplishments what I can offer. 
+                        After many years of training, work experience and
+                        learning, I always asked myself: Is this the best
+                        version of myself and the best I can give;
+                        through my accomplishments what I can offer.
                     </p>
                     <p>
                         Now I found the way: BIG TRIUMPH concept!
@@ -196,8 +154,8 @@ include('../includes/header.php');
                 <div class="row">
                     <div class="member_image">
                         <img data-interchange="
-                             [../dist/images/our-team/ivana-zdravkovic-305.jpg, (small)], 
-                             [../dist/images/our-team/ivana-zdravkovic-305.jpg, (medium)], 
+                             [../dist/images/our-team/ivana-zdravkovic-305.jpg, (small)],
+                             [../dist/images/our-team/ivana-zdravkovic-305.jpg, (medium)],
                              [../dist/images/our-team/ivana-zdravkovic-225.jpg, (large)]"
                              alt="Ivana Zdravković" />
                     </div>
